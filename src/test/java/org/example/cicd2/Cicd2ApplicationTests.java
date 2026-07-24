2883c522f485ba69bcd60ca62107f0a12f24996e
ww <lw788686@gmail.com>
2026-07-24 05:39:49 +0000

    Initial commit

---
+++ src/test/java/org/example/cicd2/Cicd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cicd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
